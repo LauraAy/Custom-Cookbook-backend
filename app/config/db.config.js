1d49e90d3735b44174c874dc743e4ca3282b5519
laura_ay <laura.ay1564@gmail.com>
2022-12-22 17:16:22 -0800

    connected mysql and started postman testing

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     HOST: "localhost",
     USER: "root",
-    PASSWORD: "123456",
-    DB: "testdb",
+    PASSWORD: "sql4pen4e",
+    DB: "cookbookdb",
     dialect: "mysql",
     pool: {
       max: 5,
